e11d648d8637f94f9f0a5c2728f6fe451c60e0ec
Alston Dias <alston_dias@persistent.com>
2024-02-18 17:50:14 +0530

    added api for creating user.

--- ca_main.py
+++ ca_main.py
@@ -7,13 +7,13 @@ from hashlib import sha256
 import datetime
 import json
 
-from fastapi import FastAPI, File, UploadFile, BackgroundTasks, Form, Request, Response
-from fastapi.response import FileResponse, RedirectResponse, JSONResponse, HTMLResponse
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks, Form, Request, Response, HTTPException
+from fastapi.responses import FileResponse, RedirectResponse, JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 from pydantic import BaseModel
-from apscheduler.schedulers.background import BackgroundScheduler
 
+from Processor import Processor
 from lib.DatabaseWrapper import DatabaseWrapper
 from lib.ca_config import ip_address, DEFAULT_SERVER_PORT, DEFAULT_UI_PORT
 
@@ -27,3 +27,36 @@ CORS_ORIGINS = [
 ]
 
 app = FastAPI()
+processor = Processor()
+database_wrapper = DatabaseWrapper()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+app.add_middleware(GZipMiddleware)
+
+@app.post("/create-user/")
+def create_user(first_name, last_name, address_line1, city, state, country, pincode, username, password, confirm_password, email, contact_number):
+    if password != confirm_password:
+        raise HTTPException(status_code=400, detail="Passwords do not match")
+    data = processor.get_users()
+    for d in data:
+        if username == d["username"]:
+            raise HTTPException(status_code=409, detail="Username already exists")
+    return processor.create_user(
+        first_name=first_name,
+        last_name=last_name,
+        address_line1=address_line1,
+        city=city,
+        state=state,
+        country=country,
+        pincode=pincode,
+        username=username, 
+        password=password, 
+        email=email, 
+        contact_number=contact_number
+    )
