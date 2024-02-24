87e92bf8c46effb632e3b065b45b2abe7475a267
kodlean <anup_kodlekere@persistent.com>
2024-02-24 08:42:04 +0530

    first pass

--- llm.py
+++ llm.py
@@ -3,12 +3,10 @@ from ibm_watsonx_ai import APIClient
 
 wml_credentials = {
                    "url": "https://us-south.ml.cloud.ibm.com",
-                   "apikey":"***********"
+                   "apikey":"rqAIv0Uy55mNG-jW0Rn-hAHm_KOHGDSdkloGUE-Yv5Ui"
                   }
 
 client = APIClient(wml_credentials)
 
 
 model_id = "ibm/granite-13b-instruct-v1"
-
-
